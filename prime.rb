cd71541e0e29d14f22b5fe138f53ea02133823f8
boruch d <dubywider@gmail.com>
2019-03-31 21:50:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(x)
+  if x == 1
+    return true 
+  elsif x < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
+end
+
+   true
+end
